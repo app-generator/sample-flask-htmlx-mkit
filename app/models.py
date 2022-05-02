21dac0d56de9331d61d4446c8637dbbf3fd5767c
B3nhard <chumba101benard@gmail.com>
2022-05-02 09:48:59 +0300

    Added Contact Model

--- app/models.py
+++ app/models.py
@@ -3,8 +3,10 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from unicodedata import name
 from app         import db
 from flask_login import UserMixin
+from datetime import datetime
 
 class Users(db.Model, UserMixin):
 
@@ -22,6 +24,33 @@ class Users(db.Model, UserMixin):
 
     def __repr__(self):
         return str(self.id) + ' - ' + str(self.user)
+
+    def save(self):
+
+        # inject self into db session    
+        db.session.add ( self )
+
+        # commit change and save the object
+        db.session.commit( )
+
+        return self 
+
+
+class Contact(db.Model):
+    __tablename__ = 'Contact'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    message = db.Column(db.String())
+    createdAt = db.Column(db.DateTime())
+
+    def __init__(self,name,email,message):
+        self.name = name
+        self.email = email
+        self.message = message
+        self.createdAt =  datetime.now()
+
 
     def save(self):
 
